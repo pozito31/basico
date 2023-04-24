de5ace8c275016488dca5caa74a93aaae6f241b5
pozito31 <jessicapozo198715@gmail.com>
2023-04-24 17:42:14 +0200

    actualizar cambios en la aplicacion

--- src/app/components/lista-usuarios/lista-usuarios.component.ts
+++ src/app/components/lista-usuarios/lista-usuarios.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-usuarios',
   templateUrl: './lista-usuarios.component.html',
-  styleUrls: ['./lista-usuarios.component.css']
+  styleUrls: ['./lista-usuarios.component.css'],
 })
-export class ListaUsuariosComponent {
+export class ListaUsuariosComponent implements OnInit {
+  usuariosActivosObs: Observable<any>;
 
+  constructor(public chatService: ChatService) {}
+
+  ngOnInit() {
+    this.usuariosActivosObs = this.chatService.getUsuariosActivos();
+  }
 }
